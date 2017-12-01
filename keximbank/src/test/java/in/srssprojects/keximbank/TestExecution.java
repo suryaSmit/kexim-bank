90834e27456840c62d9a0cb122cb979a881d3b31
surya <surya@192.168.0.103>
2017-12-01 16:55:42 +0530

    modified test execution

--- keximbank/src/test/java/in/srssprojects/keximbank/TestExecution.java
+++ keximbank/src/test/java/in/srssprojects/keximbank/TestExecution.java
@@ -5,14 +5,8 @@ import static org.testng.Assert.assertTrue;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-import org.testng.Assert;
 import org.testng.Reporter;
 
 public class TestExecution {
